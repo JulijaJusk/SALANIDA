87a2881dd7556cb702ee434e946e0a5661ad2844
julijajuskeviciene <julija.juskeviciene@gmail.com>
2024-02-04 19:21:19 +0200

    Implement annotations of Before/After Methods

--- src/test/java/lt/julija/test/TestBase.java
+++ src/test/java/lt/julija/test/TestBase.java
@@ -1,4 +1,15 @@
 package lt.julija.test;
 
-public class TestBase {
+import lt.julija.page.Common;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public abstract class TestBase {
+    @BeforeMethod
+    public abstract void setUp();
+
+    @AfterMethod
+    public void tearDown() {
+        Common.quitDriver();
+    }
 }
