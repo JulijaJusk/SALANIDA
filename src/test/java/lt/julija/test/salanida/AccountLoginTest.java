47658ff253eaca467d0a234fbd309440e4707207
julijajuskeviciene <julija.juskeviciene@gmail.com>
2024-02-04 19:32:53 +0200

    Add setUp method AccountLoginPage.open

--- src/test/java/lt/julija/test/salanida/AccountLoginTest.java
+++ src/test/java/lt/julija/test/salanida/AccountLoginTest.java
@@ -1,5 +1,6 @@
 package lt.julija.test.salanida;
 
+import lt.julija.page.salanida.AccountLoginPage;
 import lt.julija.test.TestBase;
 import org.testng.annotations.BeforeMethod;
 
@@ -7,6 +8,6 @@ public class AccountLoginTest extends TestBase {
     @BeforeMethod
     @Override
     public void setUp() {
-
+        AccountLoginPage.open("https://salanida.lt/en/account/login?return_url=%2Faccount",8);
     }
 }
