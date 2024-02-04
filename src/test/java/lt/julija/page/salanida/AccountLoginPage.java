9fcdbc64fab6d1abdfd2144ef71fd6a587cf87c2
julijajuskeviciene <julija.juskeviciene@gmail.com>
2024-02-04 19:49:48 +0200

    Create setUp methods accept cookies and refresh page

--- src/test/java/lt/julija/page/salanida/AccountLoginPage.java
+++ src/test/java/lt/julija/page/salanida/AccountLoginPage.java
@@ -1,6 +1,17 @@
 package lt.julija.page.salanida;
 
+import lt.julija.page.Common;
+import lt.julija.page.Locator;
 import lt.julija.page.PageBase;
 
 public class AccountLoginPage extends PageBase {
+
+    public static void clickOnButtonAcceptCookies() {
+        Common.waitElementVisible(Locator.Salanida.AccountLogin.buttonAcceptCookies, 6);
+        Common.clickOnElement(Locator.Salanida.AccountLogin.buttonAcceptCookies);
+    }
+
+    public static void refreshWebPage(int sec) {
+        Common.refreshPage(sec);
+    }
 }
