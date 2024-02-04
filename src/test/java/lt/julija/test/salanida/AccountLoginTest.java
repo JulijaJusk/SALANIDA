e4236a4d423fac492b830f11c443be6854de2760
julijajuskeviciene <julija.juskeviciene@gmail.com>
2024-02-04 19:29:28 +0200

    Implement Before Method

--- src/test/java/lt/julija/test/salanida/AccountLoginTest.java
+++ src/test/java/lt/julija/test/salanida/AccountLoginTest.java
@@ -1,6 +1,12 @@
 package lt.julija.test.salanida;
 
 import lt.julija.test.TestBase;
+import org.testng.annotations.BeforeMethod;
 
 public class AccountLoginTest extends TestBase {
+    @BeforeMethod
+    @Override
+    public void setUp() {
+
+    }
 }
