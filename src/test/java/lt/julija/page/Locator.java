c2bb986a9b3788c9ef083efa6f29abe9cca33126
julijajuskeviciene <julija.juskeviciene@gmail.com>
2024-02-04 19:50:29 +0200

    Add Locator for buttonAcceptCookies

--- src/test/java/lt/julija/page/Locator.java
+++ src/test/java/lt/julija/page/Locator.java
@@ -1,4 +1,12 @@
 package lt.julija.page;
 
+import org.openqa.selenium.By;
+
 public class Locator {
+    public static class Salanida {
+        public static class AccountLogin {
+
+            public static By buttonAcceptCookies = By.xpath("/html/body/div[2]/div");
+        }
+    }
 }
