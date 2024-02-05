334806b1e9e1e741c745d99d8f7a5a4ef78b8e93
julijajuskeviciene <julija.juskeviciene@gmail.com>
2024-02-05 11:46:30 +0200

    Implement TestListener in TestBase class

--- src/test/java/lt/julija/test/TestBase.java
+++ src/test/java/lt/julija/test/TestBase.java
@@ -1,15 +1,18 @@
 package lt.julija.test;
 
 import lt.julija.page.Common;
+import lt.julija.util.TestListener;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 
+@Listeners(TestListener.class)
 public abstract class TestBase {
     @BeforeMethod
     public abstract void setUp();
 
     @AfterMethod
     public void tearDown() {
-        Common.quitDriver();
+ //       Common.quitDriver();
     }
 }
