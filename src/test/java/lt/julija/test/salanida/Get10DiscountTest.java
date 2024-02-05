04d78bddd7cc932a81920eca1d175138895734ee
julijajuskeviciene <julija.juskeviciene@gmail.com>
2024-02-05 12:41:58 +0200

    Get10DiscountTest class inherit TestBase class

---
+++ src/test/java/lt/julija/test/salanida/Get10DiscountTest.java
@@ -0,0 +1,12 @@
+package lt.julija.test.salanida;
+
+import lt.julija.test.TestBase;
+import org.testng.annotations.BeforeMethod;
+
+public class Get10DiscountTest extends TestBase {
+    @BeforeMethod
+    @Override
+    public void setUp() {
+
+    }
+}
