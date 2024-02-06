3c10ee013a4fc9047de3b65da32ecc546428c97c
julijajuskeviciene <julija.juskeviciene@gmail.com>
2024-02-06 14:40:56 +0200

    Add AddNewAccountAddress setUp method

--- src/test/java/lt/julija/page/salanida/AddNewAccountAddressPage.java
+++ src/test/java/lt/julija/page/salanida/AddNewAccountAddressPage.java
@@ -1,7 +1,42 @@
 package lt.julija.page.salanida;
 
+import lt.julija.page.Common;
+import lt.julija.page.Locator;
 import lt.julija.page.PageBase;
 
 public class AddNewAccountAddressPage extends PageBase {
+
+    public static void refreshWebPage(int sec) {
+        Common.refreshPage(sec);
+    }
+
+    public static void clickOnButtonAcceptCookies() {
+        Common.waitElementVisible(Locator.Salanida.AccountLogin.buttonAcceptCookies, 6);
+        Common.clickOnElement(Locator.Salanida.AccountLogin.buttonAcceptCookies);
+    }
+
+    public static void enterEmail(String email) {
+        Common.waitElementVisible(Locator.Salanida.AccountLogin.inputEmail, 8);
+        Common.sendKeysToElement(Locator.Salanida.AccountLogin.inputEmail, email);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void enterPassword(String password) {
+        Common.waitElementIsEnabledCustomised(Locator.Salanida.AccountLogin.inputPassword, 8);
+        Common.sendKeysToElement(Locator.Salanida.AccountLogin.inputPassword, password);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void clickOnButtonLogin() {
+        Common.clickOnElement(Locator.Salanida.AccountLogin.buttonLogin);
+    }
 
 }
--- src/test/java/lt/julija/test/salanida/AddNewAccountAddressTest.java
+++ src/test/java/lt/julija/test/salanida/AddNewAccountAddressTest.java
@@ -1,5 +1,7 @@
 package lt.julija.test.salanida;
 
+import lt.julija.page.salanida.AccountLoginPage;
+import lt.julija.page.salanida.AddNewAccountAddressPage;
 import lt.julija.test.TestBase;
 import org.testng.annotations.BeforeMethod;
 
@@ -7,6 +9,14 @@ public class AddNewAccountAddressTest extends TestBase {
     @BeforeMethod
     @Override
     public void setUp() {
+        String email = "julija.demoqa@gmail.com";
+        String password = "SalanidaVilnius";
+        AddNewAccountAddressPage.open("https://salanida.lt/en/account/login?return_url=%2Faccount",8);
+        AddNewAccountAddressPage.clickOnButtonAcceptCookies();
+        AddNewAccountAddressPage.refreshWebPage(7);
+        AddNewAccountAddressPage.enterEmail(email);
+        AddNewAccountAddressPage.enterPassword(password);
+        AddNewAccountAddressPage.clickOnButtonLogin();
 
     }
 }
