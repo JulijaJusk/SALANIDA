a617cd10dbd15f65faf1372433363a68416d7868
julijajuskeviciene <julija.juskeviciene@gmail.com>
2024-02-07 07:46:25 +0200

    AddItemIntoCartTest implement setUp Method

--- src/test/java/lt/julija/test/salanida/AddItemIntoCartTest.java
+++ src/test/java/lt/julija/test/salanida/AddItemIntoCartTest.java
@@ -1,6 +1,12 @@
 package lt.julija.test.salanida;
 
 import lt.julija.test.TestBase;
+import org.testng.annotations.BeforeMethod;
 
 public class AddItemIntoCartTest extends TestBase {
+    @BeforeMethod
+    @Override
+    public void setUp() {
+
+    }
 }
